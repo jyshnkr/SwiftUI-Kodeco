da4b867d357112d8ea9ffb09510a66abcfaecba6
JayaShankar Mangina <79549340+jyshnkr@users.noreply.github.com>
2022-11-20 17:54:14 -0800

    Create BullseyeApp.swift

---
+++ Bullseye/Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by JayaShankar Mangina on 11/20/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
